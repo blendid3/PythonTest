7b3ba287ed6549a8582b1b691a205d9c37aaa3c7
blendid3 <zhqblendid@gmail.com>
2021-03-02 22:34:22 -0800

    commit System out timer update

---
+++ SystemOut/print.py
@@ -0,0 +1,8 @@
+print("%s %s do this" %(12, "Bob"))
+i = 3;
+word = 'test{0}'.format(i)
+# bytes.
+print(word)
+
+## bytes b'input\n' # bytes字节符，打印以b开头 使用utf-8
+# reading:https://blog.csdn.net/qq_16234613/article/details/79448203
